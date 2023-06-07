8ad269ad704a5be9877e715aa55d4ed9feed329e
Arthur Suzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-06-07 14:30:17 -0300

    Add files via upload

---
+++ controladores/informacoes.py
@@ -0,0 +1,10 @@
+import os 
+from colorama import init, Fore, Style
+init()
+os.system("cls")
+def informacoes():
+    print(Fore.WHITE + Style.BRIGHT +"-"*10,"Sobre o propósito","-"*10)
+    print("O Manguefy tem como grande objetivo disseminar\ncultura e turismo por meio do Manguebeat.\nProcurando democratizar o conhecimento e\nacesso a cultura para a população.")
+
+    print("")
+informacoes()
