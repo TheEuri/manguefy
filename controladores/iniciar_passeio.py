37e767c365f231d1433ef80c67ed9145d1882962
Eurivaldo Filho <eurivaldojvf@gmail.com>
2023-06-12 02:28:03 -0300

    comecar funcao iniciar passeio

--- controladores/iniciar_passeio.py
+++ controladores/iniciar_passeio.py
@@ -1,3 +1,236 @@
+from time import sleep
+import os
+from dotenv import load_dotenv
+import googlemaps
+import folium
+import polyline
+from datetime import datetime
+
+load_dotenv()
+print(os.environ.get('GOOGLE_API_KEY'))
+
+gmaps = googlemaps.Client(key=os.environ.get('GOOGLE_API_KEY'))
+
+
+
+
+rotas = {
+    '1': {
+      'nome': 'Manguebeat',
+      'pontos': [],
+      'quilometragem': 10
+    },
+    '2': {
+      'nome': 'Artesanato',
+      'pontos': [],
+      'quilometragem': 15
+    },
+    '3': {
+      'nome': 'dos Poetas',
+      'pontos': [],
+      'quilometragem': 5
+    },
+    '4': {
+      'nome': 'do carnaval',
+      'pontos': [],
+      'quilometragem': 20
+    },
+    '5': {
+      'nome': 'Praiera',
+      'pontos': [],
+      'quilometragem': 18
+    },
+    '6': {
+      'nome': 'da lama ao caos',
+      'pontos': [],
+      'quilometragem': 8
+    }
+  }
+
+def limpar_tela():
+    # Se for em um dispositivo linux, usa o clear, caso não, usa cls.
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def modo_locomocao():
+    while True:
+      limpar_tela()
+      print('-'*15, 'Qual será seu modo de locomoção?', '-'*15)
+      print('\n1 - A pé')
+      print('\n2 - De carro')
+      print('\n3 - De bicicleta')
+      print('\n4 - Voltar\n')
+      print('-'*60)
+      opcao = input("\nEscolha uma opção: ")
+
+      limpar_tela()
+      if opcao == '1':
+        print('\nVocê escolheu a opção 1 - A pé\n')
+        confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+        if confirma == 's':
+          print('\nModo de locomoção definido como a pé\n')
+          sleep(2)
+          return 1
+        else:
+          print('\nVoltando para o menu...\n')
+          sleep(2)
+          continue
+      elif opcao == '2':
+        print('\nVocê escolheu a opção 2 - De carro\n')
+        confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+        if confirma == 's':
+          print('\nModo de locomoção definido como de carro\n')
+          sleep(2)
+          return 2
+        else:
+          print('\nVoltando para o menu...\n')
+          sleep(2)
+          continue
+      elif opcao == '3':
+        print('\nVocê escolheu a opção 3 - De bicicleta\n')
+        confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+        if confirma == 's':
+          print('\nModo de locomoção definido como de bicicleta\n')
+          sleep(2)
+          return 3
+        else:
+          print('\nVoltando para o menu...\n')
+          sleep(2)
+          continue
+      elif opcao == '4':
+        print('\nVoltando para o menu...\n')
+        sleep(2)
+        return False
+
+def escolher_tipo_rota():
+  while True:
+    limpar_tela()
+    print('-'*15, 'Escolha o tipo de rota', '-'*15)
+    print('\n1 - Rota definida')
+    print('\n2 - Rota personalizada')
+    print('\n3 - Voltar\n')
+    print('-'*60)
+    opcao = input("\nEscolha uma opção: ")
+
+    limpar_tela()
+    if opcao == '1':
+      print('\nVocê escolheu a opção 1 - Rota definida\n')
+      confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+      if confirma == 's':
+        print('\nVocê escolheu rota definida.\n')
+        sleep(2)
+        return 1
+      else:
+        print('\nVoltando para o menu...\n')
+        sleep(2)
+        continue
+    elif opcao == '2':
+      print('\nVocê escolheu a opção 2 - Rota personalizada\n')
+      confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+      if confirma == 's':
+        print('\nVocê escolheu rota personalizada.\n')
+        sleep(2)
+        return 2
+      else:
+        print('\nVoltando para o menu...\n')
+        sleep(2)
+        continue
+    elif opcao == '3':
+      print('\nVoltando para o menu...\n')
+      sleep(2)
+      return False
+    
+def selecionar_rota():
+  while True:
+    limpar_tela()
+    print('-'*15, 'Escolha uma rota', '-'*15)
+    for rota in rotas:
+      print(f'{rota} - Rota {rotas[rota]["nome"]} ({rotas[rota]["quilometragem"]}km)')
+    print('\n7 - Voltar\n')
+    print('-'*60)
+    opcao = input("\nEscolha uma opção: ")
+
+    limpar_tela()
+    opcao_valida = False
+    for rota in rotas:
+      if opcao == rota:
+        opcao_valida = True
+        print(f'\nVocê escolheu a opção {rota} - Rota {rotas[rota]["nome"]} ({rotas[rota]["quilometragem"]}km)\n')
+        confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+        if confirma == 's':
+          print(f'\nVocê escolheu a rota {rota} - Rota {rotas[rota]["nome"]} ({rotas[rota]["quilometragem"]}km)\n')
+          sleep(2)
+          return rota
+        else:
+          print('\nVoltando para o menu...\n')
+          sleep(2)
+          continue
+    if opcao == '7':
+      print('\nVoltando para o menu...\n')
+      sleep(2)
+      return False
+    if opcao_valida == False:
+      print('\nOpção inválida, tente novamente.\n')
+      sleep(2)
+      continue
+
+def pedir_endereco():
+  while True:
+    limpar_tela()
+    print('-'*15, 'Digite o endereço', '-'*15)
+    print('\nExemplo: Rua dos Bobos, 0 - São Paulo')
+    endereco = input("\nDigite o endereço: ")
+
+    limpar_tela()
+    print(f'Você digitou o endereço {endereco}\n')
+    confirma = input('Digite S para confirmar ou N para cancelar: ').lower()
+    if confirma == 's':
+      print(f'\nVocê confirmou o endereço {endereco}\n')
+      print('Carregando...')
+      geocode = gmaps.geocode(endereco)
+      if len(geocode) == 0:
+        print('\nEndereço não encontrado, tente novamente.\n')
+        sleep(2)
+        continue
+      else:
+        limpar_tela()
+        print('-'*15, 'Endereço encontrado','-'*15,)
+        print(geocode[0]['formatted_address'])
+        print('-'*51)
+        
+        print('\nEsta é a localização correta?')
+        confirma = input('\nDigite S para confirmar ou N para cancelar: ').lower()
+        if confirma == 's':
+          print('\nEndereço confirmado.\n')
+          sleep(2)
+          return [geocode[0]['geometry']['location']['lat'], geocode[0]['geometry']['location']['lng']]
+        else:
+          print('\nVoltando para o menu...\n')
+          sleep(2)
+          continue
+    else:
+      print('\nVoltando para o menu...\n')
+      sleep(2)
+      continue
+      
 
 def iniciar_passeio(usuario):
-    print("Iniciando passeio")
+    while True:
+      modo_locomocao_escolhido = modo_locomocao()
+      if modo_locomocao_escolhido == False:
+        return
+      tipo_rota_escolhido = escolher_tipo_rota()
+      if tipo_rota_escolhido == False:
+        return
+      if tipo_rota_escolhido == 1:
+        rota_escolhida = selecionar_rota()
+        if rota_escolhida == False:
+          return
+        rota = rotas[rota_escolhida]
+        limpar_tela()
+        print(f'\nVocê escolheu a rota {rota_escolhida} - Rota {rota["nome"]} ({rota["quilometragem"]}km)\n')
+
+        input('\nPressione ENTER para iniciar o passeio...')
+        limpar_tela()
+
+        coordenadas_saida = pedir_endereco()
+        break
--- main.py
+++ main.py
@@ -1,245 +1,26 @@
-import os
-import time
-import geocoder
+from controladores import autenticacao
+from controladores import localizacao
+from controladores import sobre
+from controladores.menu_principal import menu_principal
 
-def inicio():
-    print("Seja bem vindo ao MANGUEFY")
-    time.sleep(2)
-    os.system("cls")
-    print("O Manguefy é um aplicativo projetado para você conhecer mais da cultura recifense e do\nManguebeat, movimento cultural extremamente significativo.\nAqui você vai poder fazer rotas interativas e ter acesso a mídias que se conectam por meio da geolocalização com a rota que você estiver fazendo.\nOs áudios disponibilizados têm relação direta com os locais que você estiver, tornando a experiência de conhecer a cultura mais dinámica e acessível no dia a dia.\n")
-    avanc=input("Avançar? ").lower
-    if avanc==("sim"):
-        return
+while True:
+    # sobre.boasvindas()
 
-def limparTela():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    
-def local():
-    global lat, lng
-    os.system("cls")
-    print("Entenda melhor a nossa proposta: ")
-    print("explicação")
-    ask=input("Você autoriza o uso? ").lower
-    if ask==("sim"):
-        g = geocoder.ip('me')
-        if g.ok:
-            lat, lng = g.latlng
-        else:
-            print("Falha ao se conectar ao servidor")
-    else:
-        print("Para usar o nosso app será necessário autorizar a localização \nnas configurações depois.")
+    # # Mostra o menu de cadastro e login
+    # usuario_atual = autenticacao.menu_cadastro_login()
 
-def registrar():
-    limparTela()
-    print("{:-^35}".format(" Tela de registro "))
-    usuario = input("Digite o nome do usuário: ")
-    senha = input("Digite a senha: ")
-
-    try:
-        with open('armazenamento_cadastros.txt', 'a') as cd:
-            cd.write(usuario + '|' + senha + '\n')
-        print("Usuário cadastrado.")
-    except IOError:
-        print("Erro ao tentar registrar o usuário.")
-
-    time.sleep(2)
-
-def login():
-    global usuario_input
-    limparTela()
-    print("{:-^35}".format(" Tela de login "))
-    usuario_input = input("Digite o nome do usuário: ")
-    senha_input = input("Digite a senha: ")
-
-    try:
-        with open('armazenamento_cadastros.txt', 'r') as cd:
-            usuarios = cd.readlines()
-    except FileNotFoundError:
-        print("Você precisa se registrar primeiro.")
-        time.sleep(2)
-        return False
-
-    for usuario in usuarios:
-        cd_usuario, cd_senha = usuario.strip().split('|')
-
-        if cd_usuario == usuario_input and cd_senha == senha_input:
-            limparTela()
-            print("{:-^35}".format(" Tela de login "))
-            print("Você está logado!")
-            time.sleep(2)
-            return True
-    limparTela()
-    print("{:-^35}".format(" Tela de login "))
-    print("Usuário ou senha inválido.")
-    time.sleep(2)
-    return False
-
-def conta():
-    os.system("cls")
-    print(usuario_input)
-    print("E-mail e telefone")
-    print("Cnfigurações")
-    print("Historico de rotas")
-    print("Rotas favoritas")
-
-def menu():
-    while True:
-        limparTela()
-        print("{:-^35}".format(" Menu principal "))
-        print()
-        print("1. Registrar")
-        print("2. Login")
-        print()
-        print("3. Sair")
-        print()
-        print("-" * 35)
-        opcao = input("Escolha uma opção: ")
-
-        if opcao == '1':
-            registrar()
-        elif opcao == '2':
-            login()
-        elif opcao == '3':
-            print("Saindo...")
-            break
-        else:
-            print("Opção inválida.")
-import os
-from colorama import init, Fore, Style
-
-init()
-os.system("cls")
-
-def informacoes():
-    w = " "
-    while w != "x":
-        print("-" * 10, "Sobre o propósito", "-" * 10)
-        print("O Manguefy tem como grande objetivo disseminar\ncultura e turismo por meio do Manguebeat.\nProcurando democratizar o conhecimento e\nacesso a cultura para a população.")
-
-        print("-" * 10, "Funcionamento do aplicativo", "-" * 10)
-        print("Por meio da geolocalização o app sugere rotas\ne faz uma curadoria\n ")
-
-        print("Pergunta frequente")
-        print("1. Como escolho rotas definidas?")
-        print("2. Voltar")
-        entrada = input("Digite o número para onde você deseja continuar: ")
-        
-        try:
-            if int(entrada) == 1:
-                os.system("cls")
-                print("Após clicar em 'Rota definida', você terá\nacesso às opções de rotas. É só escolher a que desejar e\niniciar o seu passeio.")
-        except ValueError:
-            continue
-        
-        if int(entrada) == 2:
-            os.system("cls")
-            w = 'x'
-
-
-def finalizado():
-    print("Vcoê finalizou seu passeio")
-    print("Status")
-    print("Distancia\nDuração\nMidias ativadas")
-    time.sleep(6)
-    limparTela()
-    print("Você finalizou seu passeio!")
-    print("Midias ativadas")
-    ask=input("Deseja sair")
-
-    
-
-def passeio():
-    while True:
-        limparTela()
-        print("X min\nX km - Tempo até o proximo ponto: ")
-        ask=("1 - Conta\n2- Sair\n3 - Menu da musica")
-        if ask == "1":
-            conta()
-        if ask == "2":
-            break
-        if ask== "3":
-            print("Nome da musica\nAutores \nDuração\nPausar")
-            time.sleep(20)
-            finalizado()
-            break
-
-    menu()
-            
-
-def configpasseio():
-    opcao="1"
-    while opcao == "1" or "2" or "3":
-        print("Qual será a forma de passeio?")
-        print("1 - A pé")
-        print("2 - De carro")
-        print("3 - De de bicicleta")
-        opcao = int(input("Qual a sua opção? "))
-        if opcao == 1:
-            os.system("cls")
-            print("Você escolheu a opção 1 - A pé")
-            break
-        elif opcao == 2:
-            os.system("cls")
-            print("Você escolheu a opção 2 - De carro")
-            break
-        elif opcao == 3:
-            os.system("cls")
-            print("Você escolheu a opção 3 - De bicicleta")
-            break
-        else:
-            os.system("cls")
-            print("Opção inválida!")
-
-    print ("\nFazer um passeio \nComo você quer seguir sua rota?")
-    print("1 - Rota definida")
-    print("2 - Rota personalizada")
-    opcao1 = int(input("Qual a sua opção? "))
-    os.system("cls")
-    if opcao1 == 1:
-        opc=int(input("1 - Rota Manguebeat\n2 - Rota Artesanato\n3 - Rota dos Poetas\n4 - Rota do Carnaval\n5 - Rota do Praieira\n6 - Rota da Lama ao Caos\n7 - Sair\n"))
-        if opc == 7:
-            exit()
-    else:
-        buscar=[]
-        if opcao1 == 2:
-            while True:
-                ask=input("Opções:\n1 - Conta \n2 - Selecionar pontos\n3 - Concluir Seleção")
-                if ask=="1":
-                    conta()
-                if ask == "2":
-                    print("Selecione o pontos de parada: ")
-                    buscar.append(input(""))
-                if ask == "3":
-                    if not buscar:
-                        print("Você não fez nenhuma seleção")
-                        continue
-                    else:
-                        print("O tempo estimado de passeio é: ")
-                        print("(X km)")
-                        ask=input("Inciar passeio? [S/N]").lower
-                        if ask == "s":
-                            passeio()
-                        
-
-                else:
-                    print("Opção invalida.")
-
-   
-
+    # if usuario_atual == '\sair_programa':
+    #     break
+    # else:
+    #     loop_localizacao = True
+    #     while loop_localizacao:
+    #         localizacao.limpar_tela()
+    #         permissao = localizacao.verificar_permissao_localizacao(usuario_atual)
+    #         if permissao:
+    #             loop_localizacao = False
+    #         else:
+    #             loop_localizacao = not localizacao.pedir_permissao_localizacao(usuario_atual)
                 
-
-def telacommapa():
-    ask=input("Informações, conta ou Fazer um passeio? ").lower
-    if ask == ("informações"):
-        informacoes()
-    if ask == ("conta"):
-        conta()
-    if ask == ("passeio"):
-        informacoes()
-
-
-
-
-inicio()
-menu()
-
+        usuario_atual = '1'
+        menu_principal(usuario_atual)
+        break
